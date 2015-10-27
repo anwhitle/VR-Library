ca93c87db410b1fc6f0f5b4da56d84675090b5d5
janewsom <janewsom@umflint.edu>
2015-10-27 14:04:41 -0400

    First commit

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float baseSpeed = 2.0f; //How fast the player moves
+	public float mouseSpeed = 4.0f; //How fast the player looks around
+	
+	private float verticalRotation = 0.0f;
+
+	public float UDConstraint = 60.0f; //Restricts the head from moving up more than 60 degrees
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Rotation
+		float rotateLR = Input.GetAxis ("Mouse X") * mouseSpeed; //Gets mouse movement input
+		transform.Rotate (0, rotateLR, 0);
+
+		verticalRotation -= Input.GetAxis ("Mouse Y") * mouseSpeed; //Gets mouse movement input
+		verticalRotation = Mathf.Clamp (verticalRotation, -UDConstraint, UDConstraint); //variable to restrict followed by the values to restrict it by
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
+
+		//Movement
+		float forwardSpeed = Input.GetAxis ("Vertical") * baseSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * baseSpeed;
+
+		Vector3 speed = new Vector3 (sideSpeed, 0, forwardSpeed); //Left/Right, Up/Down, Forward/Backward
+
+		speed = transform.rotation * speed;
+
+		CharacterController cc = GetComponent<CharacterController> ();
+		cc.SimpleMove (speed);
+	}
+}
